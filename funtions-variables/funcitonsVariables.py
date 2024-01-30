b7a189d88474123a756f6326e6988af1b1e129c4
Samticode <samti8686@gmail.com>
2024-01-30 21:38:22 +0100

    yeyeye

--- funtions-variables/funcitonsVariables.py
+++ funtions-variables/funcitonsVariables.py
@@ -1,10 +1,19 @@
-name = input("What is your name? ")
+# name = input("What is your name? ")
 
 # print(f'Hello {name}!')
 
-name = name.strip().title()
+# name = name.strip().title()
 
-first, last = name.split(' ')
+# first, last = name.split(' ')
 
 # print(f'Hello {name}!')
-print(f'Hello Mr.{last}!')
+# print(f'Hello Mr.{last}!')
+
+
+# x = float(input("Enter a number for X: "))
+# y = float(input("Enter a number for Y: "))
+
+# z = round(x + y)
+# z = x / y
+
+# print(f'{z:.2f}')
