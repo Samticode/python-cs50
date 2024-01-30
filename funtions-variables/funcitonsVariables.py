4c6c1be579128ddf43f1d408261797968830c667
Samticode <samti8686@gmail.com>
2024-01-30 21:21:40 +0100

    Redd for å skippe the basics siden jeg allerede vet dette, men har en indre frykt for å miste noe informasjon som kan hjelpe meg i framtiden

--- funtions-variables/funcitonsVariables.py
+++ funtions-variables/funcitonsVariables.py
@@ -1,1 +1,10 @@
-print("Hello, world")
+name = input("What is your name? ")
+
+# print(f'Hello {name}!')
+
+name = name.strip().title()
+
+first, last = name.split(' ')
+
+# print(f'Hello {name}!')
+print(f'Hello Mr.{last}!')
